19654391a4b7fea8477cb7b8c0ba84529bc1e810
Steven de jong 🍟 <sdejong@cloudsuite.com>
2024-11-07 12:13:09 +0100

    Add python setup.py

--- Makefile
+++ Makefile
@@ -11,3 +11,9 @@ flake:
 	docker run -v ${CURDIR}:/app webcomponents-scan flake8 .
 
 mypy-flake: mypy flake
+
+shell:
+	docker run -it -v ${CURDIR}:/app webcomponents-scan /bin/sh
+
+check-installable:
+	docker run -v ${CURDIR}:/app webcomponents-scan pip install -e .
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from WebComponentsScan import __version__
+
+setup(
+    name='WebComponentsScan',
+    version=__version__,
+
+    url='https://github.com/stevendejongnl/wcs',
+    author='Steven de Jong',
+    author_email='steven@steven-dejong.nl',
+
+    py_modules=['WebComponentsScan'],
+)
